ffd7e29a1999e9101d9be180448171f7536873b3
dodinhtrung97 <dodinhtrung97@gmail.com>
2018-10-04 06:47:12 +0700

    Make ROOT_FOLDER consistent to all os's

--- src/main/java/muic/backend/project0/util/Constant.java
+++ src/main/java/muic/backend/project0/util/Constant.java
@@ -1,6 +1,8 @@
 package muic.backend.project0.util;
 
+import java.io.File;
+
 public class Constant {
 
-    public static final String ROOT_FOLDER = "./bucket/";
+    public static final String ROOT_FOLDER = new File("").getAbsolutePath() + File.separator + "bucket" + File.separator;
 }
